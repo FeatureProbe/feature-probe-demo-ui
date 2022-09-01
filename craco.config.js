4f11577c097aae65132e009b0ad24e4b1cf101c8
frankzhaopeng <poryzhao@featureprobe.com>
2022-09-01 20:53:54 +0800

    fix: craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,12 @@
 module.exports = {
   plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
   webpack: {
-    configure: {
-      ignoreWarnings: [
-        function ignoreSourcemapsloaderWarnings(warning) {
-          return (
-            warning.module &&
-            warning.module.resource.includes('node_modules') &&
-            warning.details &&
-            warning.details.includes('source-map-loader')
-          );
-        },
-      ],
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        publicPath: '/demo/'
+      }
+      return webpackConfig
     }
   }
-};
+}
