bbaf5669a9002d842d094ea3ac327cb09baa8808
frankzhaopeng <poryzhao@featureprobe.com>
2022-09-13 17:40:52 +0800

    fix: set english as default language

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,7 @@ import { createContainer } from 'unstated-next';
 import { useLocalStorage } from 'utils/hooks';
 
 export const useI18N = () => {
-  const [ i18n, setI18n ] = useLocalStorage('i18n', 'zh-CN');
+  const [ i18n, setI18n ] = useLocalStorage('i18n', 'en-US');
 
   return { 
     i18n, 
